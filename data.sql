336ca12abc7e719700bf6efe877f8c49810e1843
unknown <ibr.s.ahmat@gmail.com>
2022-07-11 21:44:31 +0300

    Insert data to animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Agumon', '2020-FEB-3', 0, true, 10.23),
+('Gabumon', '2018-NOV-15', 2, true, 8),
+('Pikavhu', '2021-JAN-7', 1, false,15.04),
+('Devimon', '2017-MAY-12', 5, true, 11);
