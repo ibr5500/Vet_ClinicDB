ace4623ff7ac2e1981d6a4354df84aa7a6ba29ea
unknown <ibr.s.ahmat@gmail.com>
2022-07-18 15:56:41 +0300

    Edit on schema file

--- data.sql
+++ data.sql
@@ -303,3 +303,24 @@ VALUES (
   ),
   ('2021-JAN-11')
 );
+
+/* project 4 */
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (
+  animals_id,
+  vet_id,
+  date_of_visit
+) SELECT * FROM (
+    SELECT id FROM animals
+  ) animals_ids,
+  ( 
+    SELECT id FROM vets
+  ) vets_ids,
+  generate_series(
+    '1980-01-01'::timestamp,
+    '2021-01-01',
+    '4 hours'
+  ) 
+  visit_timestamp;
